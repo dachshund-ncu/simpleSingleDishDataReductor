9dc08d629c0842233d1e997b56afee11c7276c73
dachshund-ncu <michaldurjasz@gmail.com>
2025-05-04 13:00:42 +0200

    - fixed ui of the smaller widgets

--- UI/fitOrderChangeWidget.py
+++ UI/fitOrderChangeWidget.py
@@ -1,6 +1,6 @@
-'''
+"""
 VERY smol widget, that holds simple UI for changing fit order
-'''
+"""
 
 from PyQt5 import QtWidgets
 from .ui_elements.customButton import custom_button
@@ -8,8 +8,44 @@ from .ui_elements.customButton import custom_button
 class changeOrder(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.setStyleSheet('''
+        QWidget {
+            background-color: #121212;
+            border-radius: 10px;
+            padding: 0px;
+            font-size: 15px;
+            margin-left: 0px;
+            margin-right: 0px;
+            margin-top: 0px;
+            margin-bottom: 0px;
+        }
+        QSpinBox {
+            background-color: rgba(255,255,255,5%);
+            color: white; /* text color */
+            padding: 4px; /* padding */
+            font-size: 15px; /* font size */
+            border-radius: 8px; /* border radius */
+            text-align: left;
+            font-family: silka;
+            padding: 8px 12px;
+        }
+        QSpinBox::up-button, QSpinBox::down-button {
+            background-color: transparent;
+            color: red; /* text color */
+            border: 1px solid rgba(255,255,255,5%);
+            padding: 2px;
+        }
+        QSpinBox::up-button:pressed, QSpinBox::down-button:pressed  {
+            background-color: #C2185B;
+        }
+        QSpinBox::up-arrow, QSpinBox::down-arrow {
+            color: white; /* text color */
+            padding: 2px;
+        }
+        ''')
         self.setVisible(False)
         self.layout = QtWidgets.QGridLayout(self)
+
         self.cancel = custom_button("Cancel")
         self.apply = custom_button("Set")
         self.textEdit = QtWidgets.QSpinBox(self)
--- UI/manualCalCoeffSetter.py
+++ UI/manualCalCoeffSetter.py
@@ -1,6 +1,6 @@
-'''
+"""
 VERY smol widget, that holds simple UI for changing cal coefficient
-'''
+"""
 from PyQt5 import QtWidgets
 from .ui_elements.customButton import custom_button
 
@@ -9,6 +9,41 @@ class changeCalCoeffWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.setVisible(False)
+        self.setStyleSheet('''
+        QWidget {
+            background-color: #121212;
+            border-radius: 10px;
+            padding: 0px;
+            font-size: 15px;
+            margin-left: 0px;
+            margin-right: 0px;
+            margin-top: 0px;
+            margin-bottom: 0px;
+        }
+        QDoubleSpinBox {
+            background-color: rgba(255,255,255,5%);
+            color: white; /* text color */
+            padding: 4px; /* padding */
+            font-size: 15px; /* font size */
+            border-radius: 8px; /* border radius */
+            text-align: left;
+            font-family: silka;
+            padding: 8px 12px;
+        }
+        QDoubleSpinBox::up-button, QDoubleSpinBox::down-button {
+            background-color: transparent;
+            color: red; /* text color */
+            border: 1px solid rgba(255,255,255,5%);
+            padding: 2px;
+        }
+        QDoubleSpinBox::up-button:pressed, QDoubleSpinBox::down-button:pressed  {
+            background-color: #C2185B;
+        }
+        QDoubleSpinBox::up-arrow, QDoubleSpinBox::down-arrow {
+            color: white; /* text color */
+            padding: 2px;
+        }
+        ''')
         self.layout = QtWidgets.QGridLayout(self)
         self.cancel = custom_button("Cancel")
         self.apply = custom_button("Set")
